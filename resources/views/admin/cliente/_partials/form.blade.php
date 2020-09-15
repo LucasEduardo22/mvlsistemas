e49dbd6c19889ff21bedb8910ee76c966d62452a
JoaquimLP <joaquimlp2013@gmail.com>
2020-09-14 21:57:45 -0300

    Fix commit

--- resources/views/admin/cliente/_partials/form.blade.php
+++ resources/views/admin/cliente/_partials/form.blade.php
@@ -1,4 +1,4 @@
-{{-- <div class="form-row">
+<div class="form-row">
     <div class="form-group col-md-1">
         <div class="form-check">
             <input class="form-check-input cpf-cnpj" type="radio" name="cnpj_cpf" id="gridRadios2"
@@ -18,11 +18,11 @@
             </label>
         </div>
     </div>
-</div> --}}
+</div> 
 <div class="form-row">
     <div class="form-group col-md-6">
         <label for="_nome">Nome / Fantasia</label>
-        <input type="text" name="nome" class="form-control @error('nome') is-invalid @enderror" id="_nome" value="{{old('nome', !empty($cliente->nome) ? $cliente->nome : '')}}" placeholder="Nome / fantasia">
+        <input type="text" name="nome" class="form-control text_maiusculo @error('nome') is-invalid @enderror" id="_nome" value="{{old('nome', !empty($cliente->nome) ? $cliente->nome : '')}}" placeholder="Nome / fantasia">
         @error('nome')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -31,7 +31,7 @@
     </div>
         <div class="form-group col-md-6">
         <label for="_razao_social">Razão Social</label>
-        <input type="text" name="razao_social" class="form-control  @error('razao_social') is-invalid @enderror" id="_razao_social" value="{{old('razao_social', !empty($cliente->razao_social) ? $cliente->razao_social : '')}}" placeholder="Razão social">
+        <input type="text" name="razao_social" class="form-control text_maiusculo  @error('razao_social') is-invalid @enderror" id="_razao_social" value="{{old('razao_social', !empty($cliente->razao_social) ? $cliente->razao_social : '')}}" placeholder="Razão social">
         @error('razao_social')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -49,7 +49,7 @@
     </div>
     <div class="form-group col-md-6">
         <label for="_classificacao">Classificação</label>
-        <input type="text" name="classificacao" class="form-control @error('classificacao') is-invalid @enderror" value="{{old('classificacao', !empty($cliente->classificacao) ? $cliente->classificacao : '')}}" id="_classificacao">
+        <input type="text" name="classificacao" class="form-control text_maiusculo @error('classificacao') is-invalid @enderror" value="{{old('classificacao', !empty($cliente->classificacao) ? $cliente->classificacao : '')}}" id="_classificacao">
         @error('classificacao')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -97,7 +97,7 @@
     </div>
     <div class="form-group col-md-3 mb-3">
         <label for="_ie">IE</label>
-        <input type="text" name="ie" class="form-control @error('ie') is-invalid @enderror" id="_ie" value="{{old('ie', !empty($cliente->ie) ? $cliente->ie : '')}}" placeholder="999.99999-99">
+        <input type="text" name="ie" class="form-control text_maiusculo @error('ie') is-invalid @enderror" id="_ie" value="{{old('ie', !empty($cliente->ie) ? $cliente->ie : '')}}" placeholder="999.99999-99">
         @error('ie')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -117,7 +117,7 @@
 <div class="form-row">
     <div class="form-group col-md-4">
         <label for="_estado">Estado</label>
-        <input type="text" name="estado" class="form-control @error('estado') is-invalid @enderror" value="{{old('estado', !empty($cliente->estado) ? $cliente->estado : '')}}" id="_estado">
+        <input type="text" name="estado" class="form-control text_maiusculo @error('estado') is-invalid @enderror" value="{{old('estado', !empty($cliente->estado) ? $cliente->estado : '')}}" id="_estado">
         @error('estado')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -126,7 +126,7 @@
     </div>
     <div class="form-group col-md-4">
         <label for="_cidade">Cidade</label>
-        <input type="text" name="cidade" class="form-control @error('cidade') is-invalid @enderror" value="{{old('cidade', !empty($cliente->cidade) ? $cliente->cidade : '')}}" id="_cidade">
+        <input type="text" name="cidade" class="form-control text_maiusculo @error('cidade') is-invalid @enderror" value="{{old('cidade', !empty($cliente->cidade) ? $cliente->cidade : '')}}" id="_cidade">
         @error('cidade')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -135,7 +135,7 @@
     </div>
     <div class="form-group col-md-4">
         <label for="_bairro">Bairro</label>
-        <input type="text" name="bairro" class="form-control @error('bairro') is-invalid @enderror" value="{{old('bairro', !empty($cliente->bairro) ? $cliente->bairro : '')}}" id="_bairro">
+        <input type="text" name="bairro" class="form-control text_maiusculo @error('bairro') is-invalid @enderror" value="{{old('bairro', !empty($cliente->bairro) ? $cliente->bairro : '')}}" id="_bairro">
         @error('bairro')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -147,7 +147,7 @@
 <div class="form-row">
     <div class="form-group col-md-6">
         <label for="_endereco">Endereço</label>
-        <input type="text" name="endereco" class="form-control @error('endereco') is-invalid @enderror" value="{{old('endereco', !empty($cliente->endereco) ? $cliente->endereco : '')}}" id="_endereco">
+        <input type="text" name="endereco" class="form-control text_maiusculo @error('endereco') is-invalid @enderror" value="{{old('endereco', !empty($cliente->endereco) ? $cliente->endereco : '')}}" id="_endereco">
         @error('endereco')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -165,7 +165,7 @@
     </div>
     <div class="form-group col-md-3">
         <label for="_complemento">Complemento</label>
-        <input type="text" name="complemento" class="form-control @error('complemento') is-invalid @enderror" value="{{old('complemento', !empty($cliente->complemento) ? $cliente->complemento : '')}}" id="_complemento">
+        <input type="text" name="complemento" class="form-control text_maiusculo @error('complemento') is-invalid @enderror" value="{{old('complemento', !empty($cliente->complemento) ? $cliente->complemento : '')}}" id="_complemento">
         @error('complemento')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
