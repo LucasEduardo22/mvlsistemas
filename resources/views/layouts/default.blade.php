8d3869b52b8c196bd8d1e299602a0c6866e3164b
JoaquimLP <joaquimlp2013@gmail.com>
2020-11-29 18:37:06 -0300

    Fix matéria prima

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,6 @@
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @livewireStyles
-
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" />
     <link rel="stylesheet" href="{{ asset('fonts/iconmind.css') }}">
     
@@ -93,7 +91,6 @@
             @yield('content')
         </aside>
     </div>
-    @livewireScripts
     <!-- ./wrapper -->
     <!-- Footer end -->
     <!-- SCRIPTS -->
